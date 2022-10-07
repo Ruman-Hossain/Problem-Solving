25864ed80cd8af9fac4c32d7cab4d9d6e69a864b
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-10-08 02:30:02 +0600

    Link List Create and Data Insert

---
+++ A2zLearning/Linklist.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    node *next;
+};
+node *root = NULL;
+void append(int data){
+    if(root==NULL){
+        root = new node();
+        root->data = data;
+        root->next = NULL;
+    }else{
+        node *currentRoot = root;
+        while(currentRoot->next != NULL){
+            currentRoot = currentRoot->next;
+        }
+        node *newNode = new node();
+        newNode->data = data;
+        newNode->next = NULL;
+        currentRoot->next = newNode;
+    }
+
+}
+
+int main(){
+    cout<<"Append Data to Link List"<<endl;
+    for(int i=1;i<=10;i+=2){
+        append(i);
+    }
+    cout<<"Print All Data From Link List"<<endl;
+    //print();
+    return 0;
+}
