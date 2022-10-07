0ade2c34915fa3c66d2106061931fccd4bdc6d43
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-10-08 03:01:17 +0600

    Delete From LinkList

--- A2zLearning/Linklist.cpp
+++ A2zLearning/Linklist.cpp
@@ -29,12 +29,36 @@ void print(){
         currentNode = currentNode->next;
     }
 }
+void deleteNode(int data){
+    node *currentNode = root;
+    node *previousNode = NULL;
+    while(currentNode->data != data){
+        previousNode = currentNode;
+        currentNode = currentNode->next;
+    }
+    if(currentNode == root){
+        node *temp = root;
+        root = root->next;
+        delete(temp);
+    }else{
+        previousNode->next = currentNode->next;
+        delete(currentNode);
+    }
+}
 int main(){
     cout<<"Append Data to Link List"<<endl;
     for(int i=1;i<=10;i+=2){
         append(i);
     }
+
     cout<<"Print All Data From Link List"<<endl;
+    print();
+
+    cout<<"Delete From Link List "<<endl;
+        deleteNode(3);
+        deleteNode(5);
+
+    cout<<"Link List After Deleting the Multiples of 3"<<endl;
     print();
     return 0;
 }
