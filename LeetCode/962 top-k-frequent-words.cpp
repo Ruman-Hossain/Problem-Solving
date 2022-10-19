e2cd6720f164509cd6208363decc2d268a156267
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-10-19 23:14:21 +0600

    Using Operator Overloading

--- LeetCode/962 top-k-frequent-words.cpp
+++ LeetCode/962 top-k-frequent-words.cpp
@@ -1,22 +1,60 @@
+// class Solution {
+// public:
+//     vector<string> topKFrequent(vector<string>& words, int k) {
+//         map<string,int>WordStore;
+//         for(auto it : words){
+//             WordStore[it]++;
+//         }
+//         multimap<int,string, greater<int> >SortedWordStore;
+//         for(auto it:WordStore){
+//             SortedWordStore.insert(make_pair(it.second,it.first));
+//         }
+//         vector<string>result;
+//         int counter=0;
+//         for(auto it:SortedWordStore){
+//             if(counter==k)
+//                 break;
+//             result.push_back(it.second);
+//             counter++;
+//         }
+//         return result;
+        
+//     }
+// };
+
+// :: // USING OPERATOR OVERLOADING // :: //
 class Solution {
+private:
+    struct Info {
+        string word;
+        int fre;
+        
+        Info(string word, int fre)
+            : word(word), fre(fre) {}
+        
+        bool operator < (const Info &other) const {
+            if (fre == other.fre) {
+                return word < other.word;
+            }
+            return fre > other.fre;
+        }
+        
+    };
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        map<string,int>WordStore;
-        for(auto it : words){
-            WordStore[it]++;
+        map<string, int> mp;
+        for (string word : words) {
+            mp[word]++;
         }
-        multimap<int,string, greater<int> >SortedWordStore;
-        for(auto it:WordStore){
-            SortedWordStore.insert(make_pair(it.second,it.first));
+        vector<Info> vec;
+        for (auto it : mp) {
+            vec.push_back({it.first, it.second});
         }
-        vector<string>result;
-        int counter=0;
-        for(auto it:SortedWordStore){
-            if(counter==k)
-                break;
-            result.push_back(it.second);
-            counter++;
+        sort(vec.begin(), vec.end());
+        vector<string> ans;
+        for (int i = 0; i < k; i++) {
+            ans.push_back(vec[i].word);
         }
-        return result;
+        return ans;
     }
 };
