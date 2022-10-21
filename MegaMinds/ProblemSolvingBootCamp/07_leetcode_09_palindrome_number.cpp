16a27b8122e4ddf27671259dc595956e0de6fd9d
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-10-21 16:26:42 +0600

    Reverse Number and Check Palindrome or not

--- MegaMinds/ProblemSolvingBootCamp/07_leetcode_09_palindrome_number.cpp
+++ MegaMinds/ProblemSolvingBootCamp/07_leetcode_09_palindrome_number.cpp
@@ -1,26 +1,35 @@
 class Solution {
+    
 private:
-    int stringPalindromeCheck(string str){
-    int i=0,j=str.size()-1;
-        while(i<=j){
-            if(str[i++]!=str[j--]){
-                return false;
-            }
-        }
-        return true; 
-    }
-    // int reverseNumber(int n){
-    //     return ;
+    //int stringPalindromeCheck(string str){
+    // int i=0,j=str.size()-1;
+    //     while(i<=j){
+    //         if(str[i++]!=str[j--]){
+    //             return false;
+    //         }
+    //     }
+    //     return true; 
     // }
+    long long ten=1;
+    long long reverseNumber(long long n){
+        if(n<=9)return n;
+        long long right = n%10;
+        long long left = reverseNumber(n/10);
+        ten*=10;
+        return right*ten+left;
+    }
 public:
     bool isPalindrome(int x) {
         //Reverse Number using Recursion
-        //return x==reverseNumber(x);
+        if(x<0) return false;
+        cout<<reverseNumber(x)<<endl;
+        ten = 1;
+        return x == reverseNumber(x);
         
         //Reverse Number Number->stream->String->Reversed
-        stringstream ss;
-        ss << x;
-        string str = ss.str();
-        return stringPalindromeCheck(str);
+        // stringstream ss;
+        // ss << x;
+        // string str = ss.str();
+        // return stringPalindromeCheck(str);
     }
 };
