e36d0fd9fc132598d8a6c889e06abc20a1873456
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-10-26 12:34:59 +0600

    First Unique Character Using UNORDERED MAP OR FREQUENCY COUNTER ARRAY

--- LeetCode/387 first-unique-character-in-a-string.cpp
+++ LeetCode/387 first-unique-character-in-a-string.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,int>ump;
+        //USING UNORDERED MAP
+        // unordered_map<char,int>ump;
+        // for(int i=0;i<s.size();i++){
+        //     ump[s[i]]++;
+        // }
+        // for(int i=0;i<s.size();i++){
+        //     if(ump[s[i]]==1)
+        //         return i;
+        // }
+        // return -1;
+        
+        //USING FREQUENCY COUNTER ARRAY
+        int counter[26]={0};
         for(int i=0;i<s.size();i++){
-            ump[s[i]]++;
+            counter[s[i]-'a']++;
         }
         for(int i=0;i<s.size();i++){
-            if(ump[s[i]]==1)
+            if(counter[s[i]-'a']==1)
                 return i;
         }
         return -1;
