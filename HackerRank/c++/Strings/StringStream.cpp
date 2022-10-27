e4ede82b1c23b986b778990c2ae7718593aa464f
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-10-27 10:39:46 +0600

    StringStream Implement

---
+++ HackerRank/c++/Strings/StringStream.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    int s;
+    char ch;
+    vector<int>result;
+    while(ss>>s){
+        result.push_back(s);
+        ss>>ch;
+    }
+    return result;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
