22ac2cd3d57de900984eab03bfc7c4984c59400c
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-10-28 09:05:36 +0600

    Counting Valleys

---
+++ HackerRank/Algorithms/Implementation/counting-valleys.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+int countingValleys(int steps, string path) {
+   int valy=0;
+   int counter=0;
+   bool down= false;
+   for(int i=0;i<path.size();i++){
+       if(path[i]=='D'){
+           counter--;
+       }else if(path[i]=='U'){
+           counter++;
+           if(counter==0)
+            valy++;
+       }
+   } 
+   return valy;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string steps_temp;
+    getline(cin, steps_temp);
+
+    int steps = stoi(ltrim(rtrim(steps_temp)));
+
+    string path;
+    getline(cin, path);
+
+    int result = countingValleys(steps, path);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
