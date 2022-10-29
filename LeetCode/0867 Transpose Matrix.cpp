a37674803e06a4358b64f30ace52f17db41e83b9
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-10-29 09:00:24 +0600

    Transpose Matrix

---
+++ LeetCode/0867 Transpose Matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<vector<int>>transposed(col,vector<int>(row));
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+};
