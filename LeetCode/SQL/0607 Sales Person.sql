5530eb8f90e72400d4a156617e86c704250936dc
Md Ruman Hossain <ruman.cse.brur@gmail.com>
2023-01-21 00:23:21 +0600

    Sales Person

---
+++ LeetCode/SQL/0607 Sales Person.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT s.name FROM salesperson AS s
+WHERE s.sales_id NOT IN (
+    SELECT o.sales_id FROM orders AS o 
+        LEFT JOIN company c ON o.com_id = c.com_id
+            WHERE c.name = 'RED')
+;
