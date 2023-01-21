135d72138e01dac64f251fb391519a78af18a0d0
Md Ruman Hossain <ruman.cse.brur@gmail.com>
2023-01-21 16:46:10 +0600

    Linked List Cycle Detection

---
+++ LeetCode/0141 Linked List Cycle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL)
+            return false;
+        ListNode *fast = head, *slow = head;
+
+        while(fast && fast ->next){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow)
+                return true;
+        }
+        return false;
+    }
+};
