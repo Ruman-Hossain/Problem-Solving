9f26ef52e93104146d2f5fa7fd9db8725ba13280
Md Ruman Hossain <ruman.cse.brur@gmail.com>
2023-01-23 00:55:20 +0600

    Game Play Analysis I

---
+++ LeetCode/SQL/0511 Game Play Analysis I.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT
+    A.player_id, LAST_VALUE(A.event_date) OVER (PARTITION BY A.player_id
+ORDER BY
+    A.event_date DESC RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_login
+FROM
+    Activity A;
